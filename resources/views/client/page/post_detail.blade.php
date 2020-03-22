29b3078842043289e59ef9370b9ea923f7fe6430
zeyar <zeyarking125@gmail.com>
2020-03-22 10:20:10 +0630

    image view in client detial blade

--- resources/views/client/page/post_detail.blade.php
+++ resources/views/client/page/post_detail.blade.php
@@ -8,6 +8,7 @@
             <div class="card">
                 <div class="card-body">
                     <h3>{{ $post->title }}</h3>
+                    <img style="text-align: center;" src="{{asset($post->image)}}">
                     <p>{{ $post->content }}</p>
                     <p>Post {{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}</p>
                     <a href="{{ url("/") }}" class="btn btn-primary"> &laquo; Back</a>
